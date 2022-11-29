fe693f259becc66ddeedba268f0b166449793a95
parksontyc <parkson.tyc.chen@gmail.com>
2022-11-29 22:39:02 +0800

    寫入檔案

--- product_tyc/main.py
+++ product_tyc/main.py
@@ -11,7 +11,14 @@ while True:
         name = input('請輸入產品名稱:')
         if name == 'q':
             break
-
+    if name == 'q':
+        break
     price = input('請輸入價格:')
     booking.append([name, price])
 print(booking)
+
+
+# 寫入檔案
+with open('booking.csv', 'w') as f:
+    for line in booking:
+        f.write(line)  # TypeError: write() argument must be str, not list
